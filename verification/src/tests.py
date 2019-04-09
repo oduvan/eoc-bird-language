5c74eaa146b065a4beeb0094a4f315479d31f705
Alexander Lyabah <a.lyabah@checkio.org>
2019-04-09 20:01:06 +0300

    first commit

---
+++ verification/src/tests.py
@@ -0,0 +1,64 @@
+TESTS = {
+    "Basics": [
+        {
+            "input": "hieeelalaooo",
+            "answer": "hello",
+        },
+        {
+            "input": "hoooowe yyyooouuu duoooiiine",
+            "answer": "how you doin",
+        },
+        {
+            "input": "aaa bo cy da eee fe",
+            "answer": "a b c d e f",
+        },
+        {
+            "input": "sooooso aaaaaaaaa",
+            "answer": "sos aaa",
+        },
+    ],
+    "Edge": [
+        {
+            "input": "aaa",
+            "answer": "a"
+        },
+        {
+            "input": "zy",
+            "answer": "z"
+        },
+        {
+            "input": "aaabucidyeeefigihoiiijukulemonoooopyqorysotauuuviwuxayyyzu ziyyyxuwivouuutesiriqopaooonimelykijaiiihigefaeeedacybuaaa",
+            "answer": "abcdefghijklmnopqrstuvwxyz zyxwvutsrqponmlkjihgfedcba"
+        },
+        {
+            "input": "aaaeeeiiiooouuuyyy",
+            "answer": "aeiouy"
+        },
+        {
+            "input": "bicydafagahajokulymonepyqarisytyvewexuzo",
+            "answer": "bcdfghjklmnpqrstvwxz"
+        },
+    ],
+    "Extra": [
+        {
+            "input": "laooorueeema iiipesiuuumo",
+            "answer": "lorem ipsum"
+        },
+        {
+            "input": "tyooo bieee ooora nyooote tiooo byeee",
+            "answer": "to be or not to be"
+        },
+        {
+            "input": "baliaaa bolaaaa boloaaa baloaaa",
+            "answer": "bla bla bla bla"
+        },
+        {
+            "input": "doooo yyyooouuu sapieeeaaaky eeenugaleiiisyhy",
+            "answer": "do you speak english"
+        },
+        {
+            "input": "iii dyooony neoooto uuunadueeerisotoaaanydy yyyooouuu",
+            "answer": "i don not understand you"
+        },
+    ]
+}
